904ea89f52b7e9f7b1afcd44bffc064964e4b08e
Iman-Abddo <aragsancabdulqaadir7@gmail.com>
2026-07-15 14:29:28 +0300

    exercise 4

---
+++ Exercise 4/script.js
@@ -0,0 +1,17 @@
+function add(x, y){
+    return x + y
+}
+let sum = add(3, 4)
+
+console.log("Sum:",sum)
+console.log(add("sum: ",9, 5))
+console.log(add("sum: ",4, 7))
+console.log(add("sum: ",1, 8))
+console.log(add("sum: ",3, 6))
+
+const sam = function(x, y){
+    return x + y
+}
+
+let result = sam(4 + 8)
+console.log("Result:", result)
