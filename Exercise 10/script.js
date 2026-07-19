bb844cd488c49cd1189cea383ac362f4b43b18ab
Iman-Abddo <aragsancabdulqaadir7@gmail.com>
2026-07-19 21:40:17 +0300

    exercise 10

---
+++ Exercise 10/script.js
@@ -0,0 +1,18 @@
+let library =[
+    {title: "To kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    },
+    {
+        title:"1984",
+        author:"George Orwell",
+        year: 1949
+    },
+    {
+        title:"The Great Gatsby",
+        author:"F. Scott Fitzgerald",
+        year:1925
+    }
+];
+console.log(library[0].title)
+console.log(library[1].author)
