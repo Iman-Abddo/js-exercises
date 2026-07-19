f334684940f9a67373bae280138516ccbe6c4c2e
Iman-Abddo <aragsancabdulqaadir7@gmail.com>
2026-07-19 23:12:49 +0300

    exercise 11

---
+++ Exercise 11/script.js
@@ -0,0 +1,9 @@
+for(let i = 1; i <= 50; i++){
+console.log(i)
+}
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("Iteration number: " + i);
+// }
+// Output: Iteration number: 0, 1, 2, 3, 4
