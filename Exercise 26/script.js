a2ecd8a28cfe2b403de82c078e73b3b081a90847
Iman-Abddo <aragsancabdulqaadir7@gmail.com>
2026-07-30 20:35:34 +0300

    exercise 26

---
+++ Exercise 26/script.js
@@ -0,0 +1,25 @@
+function func(){
+alert("Please wait to accept you")
+return {
+    id: 1, name:"Aragsan"}
+}
+
+console.log(func())
+
+function getName(callback){
+setTimeout(() => {
+    const user ={
+        id: 1, name:"Iman"    }
+
+        callback(user)
+
+}, 2000);
+}
+getName(function(user) {
+    console.log(user)
+})
+
+
+
+
+
