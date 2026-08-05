79650ae904991922c97503af97a005655a34433f
Iman-Abddo <aragsancabdulqaadir7@gmail.com>
2026-08-05 19:41:34 +0300

    exercise 33

---
+++ Exercise 33/script.js
@@ -0,0 +1,7 @@
+function changeHeader(){
+    const head = document.querySelector('#head')
+    const paragraph = document.querySelector('#text')
+
+    head.textContent = "Soo Dhawow!"
+    text.innerHTML = "Kani wa safxadeenii hore"
+}
